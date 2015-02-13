1e00083557e675baf1ce5ecefc046fdd2565d449
程巍巍 <littocats@gmail.com>
2015-02-13 16:16:12 +0800

    Initial Commit

---
+++ Crypto/Crypto.h
@@ -0,0 +1,13 @@
+//
+//  Crypto.h
+//  Crypto
+//
+//  Created by 程巍巍 on 2/13/15.
+//  Copyright (c) 2015 Littocats. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface Crypto : NSObject
+
+@end
---
+++ Crypto/Crypto.m
@@ -0,0 +1,13 @@
+//
+//  Crypto.m
+//  Crypto
+//
+//  Created by 程巍巍 on 2/13/15.
+//  Copyright (c) 2015 Littocats. All rights reserved.
+//
+
+#import "Crypto.h"
+
+@implementation Crypto
+
+@end
